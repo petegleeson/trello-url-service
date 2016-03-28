4321460ebfbc98d1e13c91a6f8fcb3d1da420b46
Pete Gleeson <pgleeson@atlassian.com>
2016-03-28 20:18:24 +1100

    inital commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/trello-url-service-standalone.jar clojure.main -m trello-url-service.web
---
+++ test/trello_url_service/web_test.clj
@@ -0,0 +1,6 @@
+(ns trello-url-service.web-test
+  (:require [clojure.test :refer :all]
+            [trello-url-service.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
