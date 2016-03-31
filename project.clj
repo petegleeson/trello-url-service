8e1b4202315d5d88b6462cae295792fa2c80626d
Pete Gleeson <pgleeson@atlassian.com>
2016-03-31 22:53:18 +1100

    can pull out titles

--- project.clj
+++ project.clj
@@ -9,11 +9,13 @@
                  [ring/ring-devel "1.2.2"]
                  [ring-basic-authentication "1.0.5"]
                  [environ "0.5.0"]
-                 [com.cemerick/drawbridge "0.0.6"]]
+                 [com.cemerick/drawbridge "0.0.6"]
+                 [clj-tagsoup/clj-tagsoup "0.3.0" :exclusions [org.clojure/clojure]]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
   :uberjar-name "trello-url-service-standalone.jar"
-  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.14.0"]]}
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.14.0"]
+                             [venantius/ultra "0.4.1"]]}
              :production {:env {:production true}} 
              :uberjar {:aot :all}})
--- test/trello_url_service/card_test.clj
+++ test/trello_url_service/card_test.clj
@@ -1,6 +1,15 @@
 (ns trello-url-service.card-test
   (:require [clojure.test :refer :all]
-            [trello-url-service.card :refer :all]))
+            [trello-url-service.card :refer :all :as card]))
 
-(deftest hello-test
-  (is (= (trello-url-service.card/sayhello) "Tests should be written") "Didn't say hello"))
+(def google "http://google.com")
+
+(deftest get-page-test
+  (is
+  	(not= (card/get-page google) nil)
+  	"Did not get any page content"))
+
+(deftest get-title-test
+	(is
+		(= "Google" (card/get-title(card/get-page google)))
+	"Did not get correct title"))
