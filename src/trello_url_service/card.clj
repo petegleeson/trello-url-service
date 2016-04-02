5db07567d877a48b014b3f0c17f5ed0d050e370b
Pete Gleeson <pgleeson@atlassian.com>
2016-04-02 18:08:37 +1100

    translate dem cards

--- src/trello_url_service/card.clj
+++ src/trello_url_service/card.clj
@@ -1,1 +1,8 @@
-(ns trello-url-service.card)
+(ns trello-url-service.card
+	(:require [cheshire.core :refer :all]
+			  [trello-url-service.html :as html]))
+
+(defn translate
+	[card]
+	(merge card { :name (html/get-title-of-page (:name card))
+				  :desc (:name card) }))
--- test/trello_url_service/card_test.clj
+++ test/trello_url_service/card_test.clj
@@ -1,3 +1,11 @@
 (ns trello-url-service.card-test
   (:require [clojure.test :refer :all]
             [trello-url-service.card :refer :all :as card]))
+
+(def test-card {:name "http://google.com" :id 123})
+(def expected-card {:name "Google" :desc "http://google.com" :id 123})
+
+(deftest translate-test
+	(is
+		(= (card/translate test-card) expected-card)
+		"Did not correctly translate card"))
